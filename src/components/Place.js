ee3441a06857d80a69338a585b534e1aad2798ac
bahacolak <bahadircolak34@gmail.com>
2022-04-15 12:43:24 +0300

    fixed

--- src/components/Place.js
+++ src/components/Place.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ProgressBar from "./ProgressBar";
-
+import "./Place.css"
 
 export default function Place() { 
 
@@ -10,16 +10,18 @@ export default function Place() {
     { bgcolor: "#6a1b9a", completed: 66.6 },
     ];
     return(
-       <div>
-           <div className="container">
+       <div className="place">
+           <div>
            {testData.map((item, idx) => (
       <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
     ))}
+           </div>
+           <div className="text--place">
              <h3>Please Enter Your Place of Birth</h3>
              <input type="text" placeholder="City/Country" />
            </div>
-           <div className="row">
-             <button onClick={() => navigate('/Loading')} className="btn btn-primary col-sm-2 offset-sm-5 ">Finish Your Survey!</button>
+           <div>
+             <button onClick={() => navigate('/Loading')} className="btn btn-primary">Finish Your Survey!</button>
            </div>
        </div>
 
